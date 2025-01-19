7b15b08f47a0cdc96a27f05ee7949d40fabf3219
Hongbo Wei <hongbo_wei@outlook.com>
2025-01-19 13:07:08 +0800

    Create utility.cpp

---
+++ beginner_level/utility_demo/utility.cpp
@@ -0,0 +1,10 @@
+#include "utility.h"
+#include <iostream>
+
+void printMessage(const std::string& message) {
+    std::cout << message << std::endl;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
